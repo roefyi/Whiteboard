488e1211ee4ceac08b525412c27befbb4bc2a700
roefyi <romansdenson@gmail.com>
2024-09-09 19:13:58 -0500

    Initial Commit

---
+++ Whiteboard/WhiteboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhiteboardApp.swift
+//  Whiteboard
+//
+//  Created by Roman Denson on 9/9/24.
+//
+
+import SwiftUI
+
+@main
+struct WhiteboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
